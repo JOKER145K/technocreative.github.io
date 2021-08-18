1df76e5dbe1750155b8c349a3a186c85dce3f6d0
Technocreative TC <67739157+JOKER145K@users.noreply.github.com>
2021-08-18 16:05:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const consol = document.getElementById("console");
+const clearbtn = document.getElementById("clear");
+
+function clear(){
+
+        
+         consol.innerHtml = "";
+      
+
+
+}
